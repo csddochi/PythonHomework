ae1a4cc55d0758cbfb6b7d4811fc86e2d17a46f9
Soryung Lee <thfudwofud@naver.com>
2015-08-26 09:49:43 +0900

    hw24-1.py Modify List, etc.

--- hw24-1.py
+++ hw24-1.py
@@ -1,18 +1,17 @@
 # 20133246 Soryung Lee
 # 3 by 3 multiplication table program
 
-L1 = range(1, 10)
-L2 = range(2, 10)
+L = range(1, 10)
 
-for x in L2[::3]:
-	for y in L1:
+for x in L[1::3]:
+	for y in L:
 		for z in range(0, 2):
 			print('{0} X {1} = {2}'.format(x, y, x*y), end='\t')
 			x = x + 1
 		
 		if x < 10:
 			print('{0} X {1} = {2}'.format(x, y, x*y))
-		if x >= 10:
+		else:
 			print()
 		x = x - 2
 	print()
